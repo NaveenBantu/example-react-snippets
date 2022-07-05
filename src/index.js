9e65c3444cea18b27e1af54a2f9f9bbaef23dda8
bantu <venkata-naveen.bantu@voith.com>
2022-07-05 14:31:28 +0200

    First implementation of ToDoList

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 
+const todoList = (<div>
+<h1>Todos</h1>
+<ul>
+    <li>
+        Prepare Dinner
+    </li>
+    <li>
+        Learn React
+    </li>
+    <li>
+        Buy socks
+    </li>
+</ul>
+</div>); 
+
+ReactDOM.render(todoList,document.getElementById('root'));
 // TODO
 //
 //  o Create one or more JSX expressions for an HTML page containing a todo
