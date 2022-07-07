4781442d2d5442143d2cc13cdbb67e935ada86bb
NaveenBantu <naveen.bantu1@gmail.com>
2022-07-07 10:19:51 +0200

    update ToDoDetails with handlers

--- src/components/TodoDetails.js
+++ src/components/TodoDetails.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function TodoDetails(props) {
 
+    const [caption, setCaption] = useState('');
+
+    const handleChange = (e) =>{
+        setCaption(e.target.value);
+        console.log("change ",e.target.value);
+    }
+
+    const handleSubmit = () => {
+        console.log('created todo ',caption)
+        alert('Created a new Todo: ' + caption);
+    }
+
     return (
         <>
-            <form>
+            <form onSubmit={() => handleSubmit()}>
                 <label for='caption'>Caption</label>
-                <input type='text' id='caption' value={props.todo.caption}/>
+                <input type='text' id='caption' value={props.todo.caption} onChange={e => handleChange(e)}/>
                 <label for='desc'>Desc.</label>
-                <input type='text' id='desc' value={props.todo.caption}/>
-                <button type='submit' onClick={() => this.props.createToDo()}>Add</button>
+                <input type='text' id='desc' value={props.todo.caption} onChange={event => handleChange(event)}/>
+                <button type='submit'>Add</button>
             </form>
         </>
     );
