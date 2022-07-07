750dc6fdaa3020cbb78939b7451a904733e40ee0
NaveenBantu <naveen.bantu1@gmail.com>
2022-07-07 09:56:54 +0200

    inital state of the exercise

--- src/components/TodoDetails.js
+++ src/components/TodoDetails.js
@@ -2,21 +2,6 @@ import React from 'react';
 
 export default function TodoDetails(props) {
 
-    // TODO
-    //
-    //  o A 'Todo' should be passed as an input property to the component.
-    //
-    //  o Return a form, which contains input fields for
-    //
-    //      o the caption of the 'Todo' and
-    //      o the description.
-    //
-    //  o Let the form have a 'Submit' button.
-    //
-    //  Don't worry that the 'Submit' button is not working. We will do that
-    //  in a later exercise!
-    //
-
     return (
         <>
             <form>
